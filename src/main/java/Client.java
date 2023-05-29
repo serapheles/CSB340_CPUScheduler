7e54e6530a160d713c34402297307fcd67dc6e8d
davidOrtega18 <davidortega1987@gmail.com>
2023-05-28 22:47:15 -0700

    My stuff

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -4,10 +4,14 @@ public class Client {
         String inputFile = "src/main/resources/input";
         boolean outputYes = true;
 
-       // FCFS f1 = new FCFS(inputFile, "FCFS_output", outputYes);
-       // f1.process();
-        Priority p1 = new Priority("src/main/resources/testinput", "Priority_output", false, new int[]{1, 2});
-        p1.process();
+//        FCFS f1 = new FCFS(inputFile, "FCFS_output", outputYes);
+//        f1.process();
+
+//        Priority p1 = new Priority("src/main/resources/testinput", "Priority_output", false, new int[]{3, 6, 5, 4, 1, 2, 8, 7});
+//        p1.process();
+
+        RR r1 = new RR();
+        r1.process();
 
 
 
